c9ce77916e6c8a2d48423edc2fad32f1f87b085c
kimdonghyun <kdh8677@naver.com>
2019-02-22 15:55:50 +0900

    System Structuralization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers = [
+  {
+    'id':'whwk528',
+    'image':'https://placeimg.com/64/64/1',
+    'name': '김동현',
+    'birthday':'910605',
+    'gender':'남자',
+    'job':'프로그래머'
+  },
+  {
+    'id':'1318dnjs',
+    'image':'https://placeimg.com/64/64/2',
+    'name': '강상원',
+    'birthday':'910605',
+    'gender':'남자',
+    'job':'배송기사'
+  },
+  {
+    'id':'3',
+    'image':'https://placeimg.com/64/64/3',
+    'name': '소나타',
+    'birthday':'020101',
+    'gender':'남자',
+    'job':'폐기물'
+  }
+]
+  
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-       <img src={logo} lat="logo" />
-        <h2>Let's develop management system</h2>
+      <div>
+        {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );})}
       </div>
     );
   }
