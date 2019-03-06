96288ebbbb0be942dc4f45e57a7a08cad35cdde8
kimdonghyun <kdh8677@naver.com>
2019-03-06 18:03:16 +0900

    CREATE Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':'1',
-            'image':'https://placeimg.com/64/64/1',
-            'name': '김동현',
-            'birthday':'910605',
-            'gender':'남자',
-            'job':'프로그래머'
-          },
-          {
-            'id':'2',
-            'image':'https://placeimg.com/64/64/2',
-            'name': '강상원',
-            'birthday':'910605',
-            'gender':'남자',
-            'job':'배송기사'
-          },
-          {
-            'id':'3',
-            'image':'https://placeimg.com/64/64/3',
-            'name': '소나타',
-            'birthday':'020101',
-            'gender':'남자',
-            'job':'폐기물'
-          }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
